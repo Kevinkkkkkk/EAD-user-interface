96ae5d810063e61fd85dbf09b23ea45140b72b82
Kevinkkkkkk <379914302@qq.com>
2019-05-06 17:20:44 +0800

    db

--- EAD user interface/login/includes/header.php
+++ EAD user interface/login/includes/header.php
@@ -37,8 +37,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 			<ul class="shopping_grid">
                 <?php
-                            $query = "SELECT username ";
-                            $query .= "FROM user_information WHERE id = $_SESSION[login_id]";
+                require_once('db.php');
+                            $query = "SELECT u_id, u_name ";
+                            $query .= "FROM user_information WHERE u_id = $_SESSION[login_id]";
+               // echo $query;
                             $result = mysqli_query($connection, $query);
                                 if (!$result) {
                                     die("query is wrong");
