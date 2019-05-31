2565c1ffbd0a6e105e88a60f40232ede00cc71de
Kevinkkkkkk <379914302@qq.com>
2019-05-31 14:40:48 +0800

    Update register.php

--- EAD user interface/register.php
+++ EAD user interface/register.php
@@ -186,10 +186,10 @@ $(this).siblings("span").show();
 				<div class="clearfix"> </div>
 				<div class="register-but">
           		<div class="register-but">
-				   <form>
+
 					   <input name="submit" type="submit" value="Sign up">
 					   <div class="clearfix"> </div>
-                    </form>
+
 				</div>
 				</div></form>
 		   </div>
